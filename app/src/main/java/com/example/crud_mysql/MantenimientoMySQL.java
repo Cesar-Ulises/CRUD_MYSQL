306d00137f15af30bd6c1401d3e087df56eb59e3
Cesar-Ulises <56010602+Cesar-Ulises@users.noreply.github.com>
2019-11-03 16:41:41 -0600

    Agregando Las Clases y Layouts necesarios

---
+++ app/src/main/java/com/example/crud_mysql/MantenimientoMySQL.java
@@ -0,0 +1,13 @@
+package com.example.crud_mysql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MantenimientoMySQL extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/crud_mysql/MantenimientoMySQL1.java
@@ -0,0 +1,13 @@
+package com.example.crud_mysql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MantenimientoMySQL1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
